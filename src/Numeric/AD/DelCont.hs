be9d45de31582fe52fa5ee9e96f93b412f67be7b
Marco Zocca <marco.zocca@unfoldml.com>
2021-07-07 17:09:49 +0200

    an interesting API emerges, but grad is still a puzzle

--- src/Numeric/AD/DelCont.hs
+++ src/Numeric/AD/DelCont.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
@@ -9,10 +10,11 @@ import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef, modifySTRef')
 
 -- transformers
 import Control.Monad.Trans.Class (MonadTrans(..))
-import Control.Monad.Trans.Cont (Cont, shift, reset, evalCont, ContT, shiftT, resetT, evalContT, runContT)
+import Control.Monad.Trans.Cont (Cont, shift, reset, evalCont, ContT(..), shiftT, resetT, evalContT, runContT)
 
 import Prelude hiding (read)
 
+-- | Dual numbers
 data D a = D a a deriving (Show)
 
 withX :: (a -> a) -> D a -> D a
@@ -26,7 +28,7 @@ instance Eq a => Eq (D a) where
 instance Ord a => Ord (D a) where
   compare (D x _) (D y _) = compare x y
 
--- from http://conway.rutgers.edu/~ccshan/wiki/blog/posts/Differentiation/
+-- | from http://conway.rutgers.edu/~ccshan/wiki/blog/posts/Differentiation/
 instance Num a => Num (D a) where
   D x x' + D y y' = D (x + y) (x' + y')
   D x x' * D y y' = D (x * y) (x' * y + x * y')
@@ -43,6 +45,7 @@ newtype AD s r a = AD { unAD ::  ContT r (ST s) a } deriving (Functor, Applicati
 runAD :: (forall s . AD s a a) -> a
 runAD go = runST (evalContT $ unAD go)
 
+
 read :: STRef s a -> AD s r a
 read r = AD $ lift $ readSTRef r
 write :: STRef s a -> a -> AD s r ()
@@ -52,19 +55,38 @@ modify r f = AD $ lift $ modifySTRef' r f
 new :: a -> AD s r (STRef s a)
 new x = AD $ lift $ newSTRef x
 
--- ST references in the AD monad
-newtype DRef s a = DRef { getDRef :: AD s a (STRef s a) }
 
--- lifting pure functions? (a -> a) -> DRef s a -> DRef s a
+-- | Does this work?
+binOp :: Num b =>
+         (D a -> D a -> (D b, b -> a -> a)) -- ^ (forward result, adjoint update)
+      -> AD s (D b) (D a)
+      -> AD s (D b) (D a)
+      -> AD s (D b) (D b)
+binOp f (AD ioa) (AD iob) = AD $ do
+  a <- ioa
+  b <- iob
+  let (w@(D xw _), g) = f a b
+  ra <- lift $ newSTRef a
+  rb <- lift $ newSTRef b
+  rc <- shiftT $ \ k -> lift $ do
+    y <- newSTRef (D xw 0)
+    (D _ yd) <- k y
+    modifySTRef' ra (withD (g yd))
+    modifySTRef' rb (withD (g yd))
+    pure w
+  lift $ readSTRef rc
 
--- -- withDRef1 :: (a -> a) -> DRef s a -> AD s a ()
--- withDRef1 f (DRef act) = do
---   ref <- act
---   x <- read ref
---   let y = f x
---   write ref y
+plus :: Num a =>
+        AD s (D a) (D a)
+     -> AD s (D a) (D a)
+     -> AD s (D a) (D a)
+plus = binOp (\a b -> (a + b, (+)))
 
+instance Num a => Num (AD s (D a) (D a)) where
+  (+) = plus
 
+-- | Sum
+--
 -- from https://www.cs.purdue.edu/homes/rompf/papers/wang-icfp19.pdf
 plusD :: Num t =>
          STRef s (D t) -> STRef s (D t) -> AD s (D t) (STRef s (D t))
@@ -82,6 +104,7 @@ plusD this that = AD $ do
     modifySTRef' that (withD (+ yd))
     pure $ dd1 + dd2
 
+-- | Product
 timesD :: Num t =>
           STRef s (D t) -> STRef s (D t) -> AD s (D t) (STRef s (D t))
 timesD this that = AD $ do
@@ -98,11 +121,22 @@ timesD this that = AD $ do
     modifySTRef' this (withD (+ (x0 * yd)))
     pure $ dd1 * dd2
 
-grad f x = runAD $ AD $ do
-  z <- lift $ newSTRef $ D x 0
-  -- reset  { f(z).d = 1.0 }
-  resetT $ lift $ modifySTRef' z (withD (const 1) . withX f) -- FIXME 
-  -- resetT $ lift $ do
-  --   z <- lift $ readSTRef zref
-  lift $ readSTRef z
+
+-- bla :: (Monad m, Num a) => (a -> m b) -> m b
+bla :: ContT r m Integer
+bla = ContT $ \k -> do
+  let
+    a = 1 + 2
+  k a
+
+
+-- grad :: Num a => (a -> a) -> a -> D a
+-- grad f x = runAD $ AD $ do
+--   z <- lift $ newSTRef $ D x 0
+--   -- reset  { f(z).d = 1.0 }
+--   resetT $ lift $ modifySTRef' z (withD (const 1) . withX f) -- FIXME update is not pure
+--   -- resetT $ \ ioa -> do
+--   --   x <- ioa
+--   --   f x
+--   lift $ readSTRef z
 
